b1ca069e743bd25b307de8505d0695ec19dafcde
emonastyrev <emonastyrev@tmg.io>
2018-09-11 11:25:22 +0300

    add price data

--- pol/main.js
+++ pol/main.js
@@ -120,6 +120,120 @@ window.onload = function() {
       [2670, 162.0, 6150]
       ];
   
+  
+  //мат под плитку без стяжки
+  const SQUARE_PRICE_NexansMat = [
+   [150, 1.0, 3213],
+   [225, 1.5, 3828],
+   [300, 2.0, 4410],
+   [375, 2.5, 5034],
+   [450, 3.0, 5595],
+   [525, 3.5, 6171],
+   [600, 4.0, 6720],
+   [750, 5.0, 7692],
+   [900, 6.0, 8622],
+   [1050, 7.0, 9570],
+   [1200, 8.0, 10539],
+   [1500, 10.0, 12429],
+   [1800, 12.0, 14409]
+   ];
+   
+   const SQUARE_PRICE_NexansFlex = [
+   [375, 2.5, 3984],
+   [450, 3.0, 4452],
+   [525, 3.5, 4878],
+   [600, 4.0, 5565],
+   [750, 5.0, 5973],
+   [900, 6.0, 6771],
+   [1050, 7.0, 7887],
+   [1200, 8.0, 8136],
+   [1500, 10.0, 10551],
+   [1800, 12.0, 11871],
+   ];
+   
+   const SQUARE_PRICE_ProfithermMat = [
+   [75, 0.5, 1452],
+   [150, 1.0, 1959],
+   [225, 1.5, 2277],
+   [300, 2.0, 2643],
+   [375, 2.5, 3030],
+   [450, 3.0, 3699],
+   [525, 3.5, 4017],
+   [600, 4.0, 4809],
+   [750, 5.0, 5145],
+   [900, 6.0, 5952],
+   [1050, 7.0, 6654],
+   [1200, 8.0, 7464],
+   [1350, 9.0, 8070],
+   [1500, 10.0, 8667],
+   [1800, 12.0, 9717]
+   ];
+   
+   const SQUARE_PRICE_ProfithermEkoMat = [
+   [80, 0.5, 1083],
+   [120, 0.75, 1164],
+   [150, 1.0, 1248],
+   [220, 1.5, 1719],
+   [300, 2.0, 2016],
+   [385, 2.5, 2199],
+   [425, 3.0, 2244],
+   [565, 3.5, 2487],
+   [600, 4.0, 3006],
+   [650, 4.5, 3144],
+   [770, 5.0, 3798],
+   [815, 5.5, 4026],
+   [935, 6.0, 4152],
+   [980, 6.5, 4251],
+   [1030, 7.0, 4398],
+   [1120, 7.5, 4437],
+   [1200, 8.0, 4878],
+   [1340, 8.5, 5280],
+   [1500, 10.0, 5484],
+   [1650, 11.0, 5697],
+   [2000, 13.0, 8304],
+   [2205, 14.0, 9288]
+   ];
+   
+const SQUARE_PRICE_ProfithermEkoFlex = [
+   [80, 0.5, 633],
+   [120, 0.75, 777],
+   [150, 1.0, 816],
+   [220, 1.5, 1053],
+   [300, 2.0, 1287],
+   [385, 2.5, 1539],
+   [425, 3.0, 1857],
+   [565, 3.5, 2145],
+   [600, 4.0, 2400],
+   [650, 4.5, 2634],
+   [770, 5.0, 2916],
+   [815, 5.5, 3222],
+   [935, 6.0, 3495],
+   [1030, 7.0, 3765],
+   [1120, 7.5, 4158],
+   [1200, 8.0, 4269],
+   [1340, 8.5, 4614],
+   [1500, 10.0, 5280],
+   [1650, 11.0, 5769],
+   [2000, 13.0, 7548],
+   [2205, 14.0, 7965]
+   ];
+ 
+ //инфракр.пленка, без стяжки, под ламинат
+  const SQUARE_PRICE_ProfithermSlim = [
+   [110, 0.5, 357],
+   [220, 1.0, 537],
+   [440, 2.0, 1077],
+   [660, 3.0, 1623],
+   [880, 4.0, 2157],
+   [1100, 5.0, 2679],
+   [1320, 6.0, 3231],
+   [1540, 7.0, 3687],
+   [1760, 8.0, 4137],
+   [1980, 9.0, 4665],
+   [2200, 10.0, 5115],
+   [3300, 15.0, 7557]
+   ];
+  
     function getPower(tp, to) {
       return POWER[tp][to];
     }
@@ -136,19 +250,8 @@ window.onload = function() {
       }
       return STEP[index];
     }
-  
-    // ближайшее четное
-    function nearestEven(value) {
-     return Math.round(value);
-     // var max = Math.floor(value);
-     // return max % 2 == 0 ? max : max - 1;
-    }
-    
-     // for(var i=0; i<10; i+=0.1)
-     // console.log(i + "~" + nearestEven(i));
     
     function nearestPower(value, lookupTable) {
-      console.log("input: " + value);
       var index = 0;
       if (value <= lookupTable[0][0]) {
         return lookupTable[0];
@@ -219,9 +322,11 @@ window.onload = function() {
     function calculatePol(square) {
       var tipPom = $('select[name=tipPom]').val();
       var tipObo = $('select[name=tipObo]').val();
-      if (tipPom === null || tipObo === null) return 0;
+      if (tipPom === null || tipObo === null) {
+          throw "Не задан тип помещения или тип обогрева";
+      }
       model.p = getPower(tipPom, tipObo);
-      console.log("p=" + model.p);
+      console.log("Мощность: " + model.p);
 
       // ------------------------------
       var recPow = square * model.p;
@@ -237,36 +342,28 @@ window.onload = function() {
     }
     
     function calculatePolPrice(power) {
-        var Nexans1r = nearestPower(power, POWER_LENGTH_Nexans1r);
-        $('#cable1').html(Nexans1r[0] + "Вт | " + Nexans1r[1] + "м | " + Nexans1r[2] + "грн.");
-        $('#cable1').parent().parent().show();
-        var Nexans2r = nearestPower(power, POWER_LENGTH_Nexans2r);
-        $('#cable2').html(Nexans2r[0] + "Вт | " + Nexans2r[1] + "м | " + Nexans2r[2] + "грн.");
-        $('#cable2').parent().parent().show();
-        var Profitherm = nearestPower(power, POWER_LENGTH_Profitherm);
-        $('#cable3').html(Profitherm[0] + "Вт | " + Profitherm[1] + "м | " + Profitherm[2] + "грн.");
-        $('#cable3').parent().parent().show();
-        var ProfithermEko = nearestPower(power, POWER_LENGTH_ProfithermEko);
-        $('#cable4').html(ProfithermEko[0] + "Вт | " + ProfithermEko[1] + "м | " + ProfithermEko[2] + "грн.");
-        $('#cable4').parent().parent().show();
+        render('#cable1', POWER_LENGTH_Nexans1r, power);
+        render('#cable2', POWER_LENGTH_Nexans2r, power);
+        render('#cable3', POWER_LENGTH_Profitherm, power);
+        render('#cable4', POWER_LENGTH_ProfithermEko, power);
     }
     
     function calculateMate(square) {
-        var priceMate = 111;
-        $('#nMat').html(calculateBySquareAndPrice(square, priceMate) + "грн.");
-        $('#nMat').parent().parent().show();
-        var priceMate1 = 222;
-        $('#pMat1').html(calculateBySquareAndPrice(square, priceMate1) + "грн.");
-        $('#pMat1').parent().parent().show();
-        var priceMate2 = 333;
-        $('#pMat2').html(calculateBySquareAndPrice(square, priceMate2) + "грн.");
-        $('#pMat2').parent().parent().show();
+        render('#nMat', SQUARE_PRICE_NexansMat, square);
+        render('#nFlex', SQUARE_PRICE_NexansFlex, square);
+        render('#pMat1', SQUARE_PRICE_ProfithermMat, square);
+        render('#pMat2', SQUARE_PRICE_ProfithermEkoMat, square);
+        render('#pFlex', SQUARE_PRICE_ProfithermEkoFlex, square);
     }
     
     function calculateIrfilm(square) {
-        var priceSlim = 444;
-        $('#slim').html(calculateBySquareAndPrice(square, priceSlim) + "грн.");
-        $('#slim').parent().parent().show();
+        render('#slim', SQUARE_PRICE_ProfithermSlim, square);
+    }
+    
+    function render(id, data, input) {
+        var row = nearestPower(input, data);
+        $(id).html(row[0] + "Вт | " + row[1] + "м | " + row[2] + "грн.");
+        $(id).parent().parent().show();
     }
   
   }
